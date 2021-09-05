630ad3a08b549d3ee60cd3a6d1167fdafd606128
Alexander Novikov <mitskifys@gmail.com>
2021-09-05 17:36:40 +0300

    update

--- reportAPI.py
+++ reportAPI.py
@@ -19,57 +19,53 @@ def strfdelta(tdelta, fmt):
     t = DeltaTemplate(fmt)
     return t.substitute(**d)
 
-wb = Workbook()
 
-ws = wb.active
+def createReport(DBController:DBConnect, telegramId:int, monthNumber:int):
+    wb = Workbook()
+    ws = wb.active
 
-ws["A1"] = "Дата"
-ws["B1"] = "Время начала"
-ws["C1"] = "Время ухода"
-ws["D1"] = "Обед"
-ws["E1"] = "Время работы"
-ws["G1"] = "Больничный (в днях)"
-ws["G4"] = "Отпуск (в днях)"
-ws["I1"] = "Всего отработано (часов)"
-ws["I4"] = "Всего соц часов"
-ws["I7"] = "Всего часов"
+    ws["A1"] = "Дата"
+    ws["B1"] = "Время начала"
+    ws["C1"] = "Время ухода"
+    ws["D1"] = "Обед"
+    ws["E1"] = "Время работы"
+    ws["G1"] = "Больничный (в днях)"
+    ws["G4"] = "Отпуск (в днях)"
+    ws["I1"] = "Всего отработано (часов)"
+    ws["I4"] = "Всего соц часов"
+    ws["I7"] = "Всего часов"
 
-connect = DBConnect()
-result = connect.getHoursAndTypeInSpecifiedMonth(338986739, 9)
+    result = DBController.getHoursAndTypeInSpecifiedMonth(telegramId, monthNumber)
 
-rowIter = 2
+    rowIter = 2
 
+    for id, date, type in result:
+        if type == 0:
+            ws.cell(row=rowIter, column=1, value=date.strftime('%d.%m.%Y'))
+            ws.cell(row=rowIter, column=2, value=date.strftime('%H:%M')).number_format = 'HH:MM'
+        elif type == 1:
+            ws.cell(row=rowIter, column=3, value=date.strftime('%H:%M')).number_format = 'HH:MM'
+            dinnerTime = connect.getDinnerTime(id)
+            ws.cell(row=rowIter, column=4, value=strfdelta(datetime.timedelta(minutes=dinnerTime), '%H:%M')).number_format = 'HH:MM'
+            startTimeCell = ws.cell(row=rowIter, column=2).coordinate
+            endTimeCell = ws.cell(row=rowIter, column=3).coordinate
+            dinnerTime = ws.cell(row=rowIter, column=4).coordinate
+            ws.cell(row=rowIter, column=5, value=f"={endTimeCell}-{startTimeCell}-{dinnerTime}").number_format = 'HH:MM'
+            rowIter += 1
 
-for id, date, type in result:
-    if type == 0:
-        ws.cell(row=rowIter, column=1, value=date.strftime('%d.%m.%Y'))
-        ws.cell(row=rowIter, column=2, value=date.strftime('%H:%M')).number_format = 'HH:MM'
-    elif type == 1:
-        ws.cell(row=rowIter, column=3, value=date.strftime('%H:%M')).number_format = 'HH:MM'
-        dinnerTime = connect.getDinnerTime(id)
-        ws.cell(row=rowIter, column=4, value=strfdelta(datetime.timedelta(minutes=dinnerTime), '%H:%M')).number_format = 'HH:MM'
-        startTimeCell = ws.cell(row=rowIter, column=2).coordinate
-        endTimeCell = ws.cell(row=rowIter, column=3).coordinate
-        dinnerTime = ws.cell(row=rowIter, column=4).coordinate
-        ws.cell(row=rowIter, column=5, value=f"={endTimeCell}-{startTimeCell}-{dinnerTime}").number_format = 'HH:MM'
-        rowIter += 1
-
-lastCell = ws.cell(row=rowIter - 1, column=5).coordinate
-ws["I2"] = f"=СУММ(E2:{lastCell})"
-ws["I2"].number_format = 'HH:MM'
+    lastCell = ws.cell(row=rowIter - 1, column=5).coordinate
+    ws["I2"] = f"=СУММ(E2:{lastCell})"
+    ws["I2"].number_format = 'HH:MM'
     
 
-cell_range = ws['A1':'E1']
-#cell_range.alignment = Alignment(horizontal="center", vertical="center")
+    cell_range = ws['A1':'E1']
+    for row in cell_range:
+        for cell in row:
+            cell.alignment = Alignment(horizontal="center", vertical="center")
 
-for row in cell_range:
-    for cell in row:
-        cell.alignment = Alignment(horizontal="center", vertical="center")
-
-dim_holder = DimensionHolder(worksheet=ws)
-for col in range(ws.min_column, ws.max_column + 1):
-    dim_holder[get_column_letter(col)] = ColumnDimension(ws, min=col, max=col, width=15)
-ws.column_dimensions = dim_holder
-
-
-wb.save("test.xlsx")
+    dim_holder = DimensionHolder(worksheet=ws)
+    for col in range(ws.min_column, ws.max_column + 1):
+        dim_holder[get_column_letter(col)] = ColumnDimension(ws, min=col, max=col, width=15)
+    ws.column_dimensions = dim_holder
+    month = datetime.date(1900, monthNumber, 1).strftime('%B')
+    wb.save(f"{month}.xlsx")
